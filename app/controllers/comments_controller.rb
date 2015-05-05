c999caca1319e065a2d2acd4df72e063a93d1476
Colm Tuite <colmtuite@gmail.com>
2015-05-05 02:38:22 +0100

    Got ajax form working like a boss.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,14 @@
 class CommentsController < ApplicationController
   def create
+    # This is just here so the create JS will work
+    @chat = Chat.new(params[:chat])
+    
     @comment = Comment.new(creation_params)
     @comment.user_id = current_user.id
     
     if @comment.save
       respond_to do |format|
-       format.js
+       format.js { render layout: false, content_type: 'text/javascript' }
        format.html
       end
     else
